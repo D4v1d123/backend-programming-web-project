47ddf5a1baa629bde6c6770189d8f6dc67a93b91
David Guerrero <daviguer890@gmail.com>
2023-05-10 11:40:27 -0500

    Se programó la inserción a la base de datosd del formulario, excepto los archivo de la publicación

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,11 +18,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const dbConexion = getFirestore();
+//Se crea la conexión a firestore
+const db_conexion = getFirestore();
 
-//, nombre_archivo, nombre_persona, url_archivo, tipo_archivo, fechaPublicacion
-
-export const crearPublicacion = (comentario) => {
-    alert(comentario);
-    
+export const crearPublicacion = (comentario, nombre_archivo, nombre_persona, url_archivo, tipo_archivo, fechaPublicacion) => {
+    addDoc(collection(db_conexion, 'publicacion'), {comentario, nombre_archivo, nombre_persona, url_archivo, tipo_archivo, fechaPublicacion});
+    alert("Funcionó");    
 }
---
+++ js/subirPublicacion.js
@@ -0,0 +1,50 @@
+import { crearPublicacion } from "./firebase.js";
+
+const formulario_publicacion = document.getElementById("formulario_publicacion");
+const info_archivo = document.getElementById("archivo").files[0];
+const ruta_archivo = document.getElementById("archivo").value;
+const nombre_usuario = "Pepito Perez", date = new Date();
+
+//Subir al servidor el archivo agregado desde el input
+function copiarArchivo() {
+    const nombreArchivo = info_archivo.name;
+    const urlDestino = 'http://127.0.0.1:5501/archivos/publicacion/' + nombreArchivo;
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', urlDestino);
+    xhr.send(info_archivo);
+}
+
+formulario_publicacion.addEventListener ('submit', (evento) => {
+    //Prevenir que la página web se recarge cuando se active el evento 'submit'
+    evento.preventDefault();
+
+    const comentario = formulario_publicacion['comentario'].value;
+    const nombre_archivo = info_archivo.name;
+    const url_archivo = "../archivos/publicacion/" + info_archivo.name;
+    const tipo_archivo = info_archivo.type;
+    const fechaPublicacion = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+
+    copiarArchivo();
+
+/*
+    const formData = new FormData();
+    formData.append('file', document.getElementById("archivo").files[0]);
+
+
+    fetch("http://127.0.0.1:5501/archivos/publicacion", {
+        method: 'POST',
+        body: formData
+      }).then(response => {
+        if (response.ok) {
+          console.log('Archivo copiado exitosamente');
+        } else {
+          console.error('Error al copiar el archivo');
+        }
+      }).catch(error => {
+        console.error('Error al realizar la solicitud: ', error);
+    });
+*/
+
+    crearPublicacion(comentario, nombre_archivo, nombre_usuario, url_archivo, tipo_archivo, fechaPublicacion);
+
+});
